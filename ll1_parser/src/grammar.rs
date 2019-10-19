b28d27be245de4efb7e917a65bbf3dad319e2cbc
Niklas Buechner <niklas.buechner@viison.com>
2019-10-19 23:05:12 +0200

    Add parser table calculation to LL(1) grammar

--- ll1_parser/src/grammar.rs
+++ ll1_parser/src/grammar.rs
@@ -70,70 +70,28 @@ impl Grammar {
     }
 
     pub fn get_parse_table(&self) -> HashMap<String, HashMap<Token, Vec<GrammarSymbol>>> {
-        let e_opening_bracket_and_id_production = vec![
-            GrammarSymbol::NonTerminal("T".to_string()),
-            GrammarSymbol::NonTerminal("E'".to_string()),
-        ];
-
-        let e_stroke_plus_production = vec![
-            GrammarSymbol::Terminal(Token::Plus),
-            GrammarSymbol::NonTerminal("T".to_string()),
-            GrammarSymbol::NonTerminal("E'".to_string()),
-        ];
-
-        let t_opening_bracket_and_id_production = vec![
-            GrammarSymbol::NonTerminal("F".to_string()),
-            GrammarSymbol::NonTerminal("T'".to_string()),
-        ];
-
-        let t_stroke_times_production = vec![
-            GrammarSymbol::Terminal(Token::Times),
-            GrammarSymbol::NonTerminal("F".to_string()),
-            GrammarSymbol::NonTerminal("T'".to_string()),
-        ];
-
-        let f_id_production = vec![GrammarSymbol::Terminal(Token::Id)];
-        let f_opening_bracket_production = vec![
-            GrammarSymbol::Terminal(Token::OpenBracket),
-            GrammarSymbol::NonTerminal("E".to_string()),
-            GrammarSymbol::Terminal(Token::ClosingBracket),
-        ];
-
-        let mut e_line = HashMap::new();
-        e_line.insert(
-            Token::OpenBracket,
-            e_opening_bracket_and_id_production.clone(),
-        );
-        e_line.insert(Token::Id, e_opening_bracket_and_id_production);
-
-        let mut e_stroke_line = HashMap::new();
-        e_stroke_line.insert(Token::ClosingBracket, Vec::new());
-        e_stroke_line.insert(Token::Plus, e_stroke_plus_production);
-        e_stroke_line.insert(Token::EndSymbol, Vec::new());
-
-        let mut t_line = HashMap::new();
-        t_line.insert(
-            Token::OpenBracket,
-            t_opening_bracket_and_id_production.clone(),
-        );
-        t_line.insert(Token::Id, t_opening_bracket_and_id_production);
-
-        let mut t_stroke_line = HashMap::new();
-        t_stroke_line.insert(Token::ClosingBracket, Vec::new());
-        t_stroke_line.insert(Token::Plus, Vec::new());
-        t_stroke_line.insert(Token::Times, t_stroke_times_production);
-        t_stroke_line.insert(Token::EndSymbol, Vec::new());
-
-        let mut f_line = HashMap::new();
-        f_line.insert(Token::Id, f_id_production);
-        f_line.insert(Token::OpenBracket, f_opening_bracket_production);
-
         let mut table = HashMap::new();
-        table.insert("E".to_string(), e_line);
-        table.insert("E'".to_string(), e_stroke_line);
-        table.insert("T".to_string(), t_line);
-        table.insert("T'".to_string(), t_stroke_line);
-        table.insert("F".to_string(), f_line);
+        self.productions.keys().for_each(|non_terminal| {
+            let mut line = HashMap::new();
+
+            let productions = self.productions.get(non_terminal).unwrap();
+            productions.iter().for_each(|production| {
+                let first = self.get_first_for_string(production);
+                first.iter().for_each(|token| {
+                    line.insert(token.clone(), production.clone());
+                });
+            });
+
+            let all_productions_first = self.get_first(non_terminal);
+            if all_productions_first.contains(&Token::EmptySet) {
+                let follow = self.get_follow(non_terminal);
+                follow.into_iter().for_each(|token| {
+                    line.insert(token, Vec::new());
+                });
+            }
+
+            table.insert(non_terminal.clone(), line);
+        });
 
         table
     }
